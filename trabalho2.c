816b89e2388830336f786767d447946a4e67903c
viniciushora <viniciuscorte@protonmail.com>
2023-06-18 18:37:22 -0300

    só falta finalizar o código e ajeitar os inputs da main que tãoestáticos

--- trabalho2.c
+++ trabalho2.c
@@ -10,15 +10,18 @@
 
 typedef struct {
 	int* numbers;
-	int* primes;
 	int max;
 	int bufferSize;
-	int primeSize;
 	int count;
+	int resourcesGenerated;
 	int maxCount;
 	int primeCount;
 	int front;
 	int rear;
+	int round;
+	int actualNumber;
+	int quantityThreads;
+	sem_t* semThreads;
 	sem_t bufferEnd;
 	pthread_mutex_t mutex;
 	pthread_cond_t full;
@@ -46,6 +49,8 @@ typedef struct {
 	int count;
 	int length;
 	pthread_mutex_t mutex;
+	sem_t printVerifier;
+	sem_t printMailer;
 } PrinterBuffer;
 
 typedef struct {
@@ -54,26 +59,45 @@ typedef struct {
 
 typedef struct {
 	int id;
-	int round;
+	int* primes;
+	int primeCount;
 	PipelineBuffer* pipeline;
 	PrinterBuffer* printerBuffer;
 } AnalyzeThread;
 
-void initializeBuffer(PipelineBuffer* buffer, int maximumSize, int bufferSize, int primeSize) {
+void initializeBuffer(PipelineBuffer* buffer, int maximumSize, int quantityThreads, int bufferSize, sem_t* semThreads) {
 	buffer->numbers = (int*)malloc(bufferSize * sizeof(int));
-	buffer->primes = (int*)malloc(primeSize * sizeof(int));
 	buffer->max = maximumSize;
 	buffer->bufferSize = bufferSize;
-	buffer->primeSize = primeSize;
+	buffer->quantityThreads = quantityThreads;
 	buffer->count = 0;
+	buffer->resourcesGenerated = 0;
 	buffer->maxCount = 0;
 	buffer->primeCount = 0;
 	buffer->front = 0;
 	buffer->rear = 0;
+	buffer->round = 0;
+	buffer->actualNumber = 0;
 	sem_init(&buffer->bufferEnd, 0, 0);
 	pthread_mutex_init(&buffer->mutex, NULL);
 	pthread_cond_init(&buffer->full, NULL);
 	pthread_cond_init(&buffer->empty, NULL);
+
+	sem_init(&semThreads[0], 0, 1);
+	for (int i = 1; i < quantityThreads; i++) {
+		sem_init(&semThreads[i], 0, 0);
+	}
+	buffer->semThreads = semThreads;
+}
+
+void initializeThread(int id, PipelineBuffer* pipeline, PrinterBuffer* printerBuffer, int primesSize, pthread_t* analyzer) {
+	AnalyzeThread analyzerArgs;
+	analyzerArgs.id = id;
+	analyzerArgs.pipeline = pipeline;
+	analyzerArgs.primes = (int*)malloc(primesSize * sizeof(int));
+	analyzerArgs.printerBuffer = printerBuffer;
+	analyzerArgs.primeCount = 0;
+	//pthread_create(&analyzer[id], NULL, analyzeNumbers, (void*)&analyzerArgs);
 }
 
 void initializePrinterBuffer(PrinterBuffer* buffer, int maximumSize) {
@@ -84,11 +108,12 @@ void initializePrinterBuffer(PrinterBuffer* buffer, int maximumSize) {
 	buffer->lastPrint = 1;
 	buffer->count = 0;
 	pthread_mutex_init(&buffer->mutex, NULL);
+	sem_init(&buffer->printMailer, 0, 1);
+	sem_init(&buffer->printVerifier, 0, 0);
 }
 
 void destroyBuffer(PipelineBuffer* buffer) {
 	free(buffer->numbers);
-	free(buffer->primes);
 	pthread_mutex_destroy(&buffer->mutex);
 	pthread_cond_destroy(&buffer->full);
 	pthread_cond_destroy(&buffer->empty);
@@ -105,6 +130,7 @@ void produceNumber(PipelineBuffer* buffer, int number) {
 	buffer->numbers[buffer->rear] = number;
 	buffer->rear = (buffer->rear + 1) % buffer->bufferSize;
 	buffer->count++;
+	buffer->resourcesGenerated++;
 
 	pthread_cond_signal(&buffer->full);
 	pthread_mutex_unlock(&buffer->mutex);
@@ -138,7 +164,7 @@ void produceResult(PrinterBuffer* buffer, int number, int id, int round, int div
 	}
 	else {
 		printf("%d CAUSED INTERNAL BUFFER OVERFLOW IN thread %d at round %d\n", number, id, round);
-		sem_post(buffer->printEnd);
+		sem_post(&buffer->printEnd);
 	}
 }
 
@@ -148,6 +174,7 @@ void* printResults(void* args) {
 	int i;
 
 	while (1) {
+		sem_wait(&buffer->printVerifier);
 		for (i = 0; i < buffer->length; i++) {
 			if (buffer->inputs[i].number == buffer->lastPrint + 1) {
 				produceResult(buffer, buffer->inputs[i].number, buffer->inputs[i].id, buffer->inputs[i].round, buffer->inputs[i].divisor);
@@ -155,15 +182,19 @@ void* printResults(void* args) {
 				buffer->deadIndex++;
 			}
 		}
+		sem_post(&buffer->printMailer);
 	}
 }
 
 void sendNumberToPrinter(PrinterBuffer* buffer, PrinterPackage numberPackage) {
+	sem_wait(&buffer->printMailer);
 	int i = buffer->count;
 
 	buffer->inputs[i] = numberPackage;
 
 	buffer->count++;
+
+	sem_post(&buffer->printVerifier);
 }
 
 void* generateNumbers(void* args) {
@@ -184,50 +215,73 @@ void* analyzeNumbers(void* args) {
 	PrinterBuffer* printerBuffer = threadArgs->printerBuffer;
 	int id = threadArgs->id;
 	PrinterPackage numberPackage;
+	int number;
+	int nextThread;
+	int returnToZero = 0;
 
 	while (1) {
-		int number = consumeNumber(buffer);
-		threadArgs->round = 0;
+		sem_wait(&buffer->semThreads[id]);
+
+		numberPackage.round = buffer->round;
+		numberPackage.id = id;
+
+		if (id == buffer->quantityThreads - 1) {
+			nextThread = 0;
+			returnToZero = 1;
+		}
+		else {
+			nextThread = id+1;
+		}
+
+		if (buffer->actualNumber == 0) {
+			number = consumeNumber(buffer);
+			buffer->actualNumber = number;
+		} else {
+			number = buffer->actualNumber;
+		}
+
+		numberPackage.number = number;
+
+		if (buffer->round + 1 > buffer->bufferSize) {
+			numberPackage.divisor = -1;
+			sendNumberToPrinter(printerBuffer, numberPackage);
+		}
 
 		int isPrime = 1;
 		numberPackage.id = id;
 		numberPackage.number = number;
 
-		for (int i = 0; i < buffer->primeCount; i++) {
-
-			if (number % buffer->primes[i] == 0) {
-				isPrime = 0;
-
-				numberPackage.divisor = buffer->primes[i];
-				numberPackage.round = threadArgs->round;
-				break;
-			}
-			threadArgs->round++;
+		if (number % threadArgs->primes[buffer->round] == 0) {
+			isPrime = 0;
+			numberPackage.divisor = threadArgs->primes[buffer->round];
 		}
 
 		if (isPrime) {
-			if (buffer->primeCount < buffer->primeSize) {
-				buffer->primes[buffer->primeCount++] = number;
+			if (threadArgs->primeCount <= buffer->round) {
+				threadArgs->primes[buffer->round] = number;
+				threadArgs->primeCount++;
 
 				numberPackage.divisor = 0;
-				numberPackage.round = threadArgs->round;
 
 				sendNumberToPrinter(printerBuffer, numberPackage);
+				buffer->round = 0;
+				buffer->actualNumber = 0;
+				sem_post(&buffer->semThreads[0]);
+			} else {
+				if (returnToZero) {
+					buffer->round++;
+				}
+				sem_post(&buffer->semThreads[nextThread]);
 			}
-			else {
-				numberPackage.divisor = -1;
-				numberPackage.round = threadArgs->round;
-			}
+		} else {
+			sendNumberToPrinter(printerBuffer, numberPackage);
+			buffer->round = 0;
+			buffer->actualNumber = 0;
+			sem_post(&buffer->semThreads[0]);
 		}
 
-		pthread_mutex_lock(&printerBuffer->mutex);
-
-		sendNumberToPrinter(printerBuffer, numberPackage);
-
-		pthread_mutex_unlock(&printerBuffer->mutex);
-
 		if (number == buffer->maxCount) {
-			sem_post(&buffer->bufferEnd);
+			sem_wait(&buffer->bufferEnd);
 		}
 	}
 }
@@ -249,8 +303,10 @@ int main(int argc, char* argv[]) {
 	int K = 3;
 	int X = 2;
 
+	sem_t* semThreads = (sem_t*)malloc(M * sizeof(sem_t));
+
 	PipelineBuffer buffer;
-	initializeBuffer(&buffer, N, K, X);
+	initializeBuffer(&buffer, N, M, X, semThreads);
 
 	PrinterBuffer printerBuffer;
 	initializePrinterBuffer(&printerBuffer, N);
@@ -259,18 +315,21 @@ int main(int argc, char* argv[]) {
 	pthread_t* analyzer = (pthread_t*)malloc(M * sizeof(pthread_t));
 	pthread_t printer;
 
-	sem_t generateEnd;
-	sem_init(&generateEnd, 0, 0);
-
 	GeneratorThread generatorArgs = { .count = 0, .max = M, .pipeline = &buffer };
 	pthread_create(&generator, NULL, generateNumbers, (void*)&generatorArgs);
 
 	PrinterThread printerArgs = { .buffer = &printerBuffer };
 	pthread_create(&printer, NULL, printResults, (void*)&printerArgs);
 
+	AnalyzeThread* analyzerArgs = (AnalyzeThread*)malloc(M * sizeof(AnalyzeThread));
 	for (int i = 0; i < M; i++) {
-		AnalyzeThread analyzerArgs = { .id = i, .pipeline = &buffer, .printerBuffer = &printerBuffer };
-		pthread_create(&analyzer[i], NULL, analyzeNumbers, (void*)&analyzerArgs);
+		analyzerArgs[i].id = i;
+		analyzerArgs[i].pipeline = &buffer;
+		analyzerArgs[i].primes = (int*)malloc(X * sizeof(int));
+		analyzerArgs[i].printerBuffer = &printerBuffer;
+		analyzerArgs[i].primeCount = 0;
+		pthread_create(&analyzer[i], NULL, analyzeNumbers, (void*)(analyzerArgs + i));
+		//initializeThread(i, &buffer, &printerBuffer, X, &analyzer);
 	}
 
 	sem_wait(&printerBuffer.printEnd);
